e009d519bcd3b0b54f722a2059261935cd5448a9
Ian Shoenberger <ian.shoenberger@fujifilm.com>
2023-09-14 10:12:26 -0400

    added icons and assets for pwa

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,41 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     vue(),
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Mediaclip Upload First',
+        short_name: 'MediaclipPOC',
+        description: 'Upload an image and generate beauty shots with mediaclip hub api',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'manifest-icon-192.maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'manifest-icon-192.maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: 'manifest-icon-512.maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'manifest-icon-512.maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
   ],
 })
