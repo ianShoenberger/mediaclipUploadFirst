325a3406ef6050d0e488d3b44860fbe61445e237
Ian Shoenberger <ian.shoenberger@fujifilm.com>
2023-09-18 11:37:48 -0400

    made the vue app use the composition api

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,67 @@
-<script>
+<script setup>
+import { ref } from 'vue'
 import { MediaclipHubApi } from './api'
 import { products } from './config'
-export default {
- data() {
-  return {
-    userImage: null,
-    beautyShots: [],
-    authenticated: false,
-    appKey: '',
-    appSecret: '',
-    mediaclipHubApi: null
-  }
- },
- methods: {
-  imageSelected(evt) {
-    const fileObj = evt.target.files[0]
-    if (!fileObj.type.startsWith("image/")) {
-      return
-    }
-    const fileReader = new FileReader()
-    fileReader.onload = (e) => {
-      this.uploadPhoto(fileObj.name, e.target.result)
-    }
-    fileReader.readAsArrayBuffer(fileObj)
-  },
-  async uploadPhoto(name, binaryString) {
-    const uploadStatus = await this.mediaclipHubApi.uploadPhoto(name, binaryString)
-    
-    if (!uploadStatus.ready) {
-      let attempt = 1
-      let delay = uploadStatus.remaining
-      let statusCheck = null
-      do {
-        statusCheck = await this.mediaclipHubApi.checkUploadStatus(uploadStatus.photoId, attempt, delay)
-        attempt += 1
-        delay = statusCheck.remaining
-      } while (statusCheck.status !== 'Done')
-    }
 
-    this.getBeautyShots(uploadStatus.photoId)
-  },
-  async getBeautyShots (photoUrn) {
-    const beautyShots = await this.mediaclipHubApi.getBeautyShots(photoUrn, products)
-    this.beautyShots = products.map((product, i) => {
-      return {
-        name: product.name,
-        url: beautyShots[i],
-        originalImage: photoUrn
-      }
-    })
-  },
-  async addToCart(productName, customerImage) {
-    const foundProduct = products.find(product => product.name === productName)
-    const newProjectId = await this.mediaclipHubApi.createProject(foundProduct, customerImage)
-    const addToCartResult = await this.mediaclipHubApi.addToCart(newProjectId, foundProduct)
-    debugger
-  },
-  async getToken() {
-    this.mediaclipHubApi = new MediaclipHubApi(this.appKey, this.appSecret)
-    await this.mediaclipHubApi.createUserToken()
-    this.authenticated = true
+const beautyShots = ref([])
+const authenticated = ref(false)
+const appKey = ref('')
+const appSecret = ref('')
+let mediaclipHubApi = null
+ 
+function imageSelected(evt) {
+  const fileObj = evt.target.files[0]
+  if (!fileObj.type.startsWith("image/")) {
+    return
   }
- }
+  const fileReader = new FileReader()
+  fileReader.onload = (e) => {
+    uploadPhoto(fileObj.name, e.target.result)
+  }
+  fileReader.readAsArrayBuffer(fileObj)
 }
+async function uploadPhoto(name, binaryString) {
+  const uploadStatus = await mediaclipHubApi.uploadPhoto(name, binaryString)
+  
+  if (!uploadStatus.ready) {
+    let attempt = 1
+    let delay = uploadStatus.remaining
+    let statusCheck = null
+    do {
+      statusCheck = await mediaclipHubApi.checkUploadStatus(uploadStatus.photoId, attempt, delay)
+      attempt += 1
+      delay = statusCheck.remaining
+    } while (statusCheck.status !== 'Done')
+  }
 
+  getBeautyShots(uploadStatus.photoId)
+}
+async function getBeautyShots (photoUrn) {
+  const rawBeautyShots = await mediaclipHubApi.getBeautyShots(photoUrn, products)
+  beautyShots.value = products.map((product, i) => {
+    return {
+      name: product.name,
+      url: rawBeautyShots[i],
+      originalImage: photoUrn
+    }
+  })
+}
+async function addToCart(productName, customerImage) {
+  const foundProduct = products.find(product => product.name === productName)
+  const newProjectId = await mediaclipHubApi.createProject(foundProduct, customerImage)
+  const addToCartResult = await mediaclipHubApi.addToCart(newProjectId, foundProduct)
+  debugger
+}
+async function getToken() {
+  mediaclipHubApi = new MediaclipHubApi(appKey.value, appSecret.value)
+  await mediaclipHubApi.createUserToken()
+  authenticated.value = true
+}
 </script>
 
 <template>
   <div v-if="authenticated" class="container">
     <input type="file" id="imagePicker" ref="imagePicker" @change="imageSelected" />
-    <img v-if="userImage" :src="userImage" />
     <div class="row row-cols-1 row-cols-md-4 g-4">
       <div v-for="beautyShot in beautyShots" :key="beautyShot.name" class="col">
         <div class="card h-100">
